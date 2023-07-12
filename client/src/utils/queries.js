3ae501dd6e6844c92acf5da41fecdf3b10cf0559
eric <eric.jeangirard@recherche.gouv.fr>
2023-07-12 11:56:29 +0200

    should instead of must

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,22 +1,23 @@
 const getQuery = ({ datasource, filters }) => {
-  const query = { size: 20, query: { bool: {} } };
+  const query = { size: 100, query: { bool: {} } };
   switch (datasource) {
   case 'bso':
     if (filters.affiliations.length > 0 || filters.authors.length > 0 || filters?.startYear || filters?.endYear) {
-      query.query.bool.must = [];
+      query.query.bool.should = [];
+      query.query.bool.filter = [];
     }
     filters.affiliations.forEach((affiliation) => {
-      query.query.bool.must.push({ match: { 'affiliations.name': { query: `"${affiliation}"`, operator: 'and' } } });
+      query.query.bool.should.push({ match: { 'affiliations.name': { query: `"${affiliation}"`, operator: 'and' } } });
     });
     filters.authors.forEach((author) => {
-      query.query.bool.must.push({ match: { 'authors.full_name': { query: `"${author}"`, operator: 'and' } } });
+      query.query.bool.should.push({ match: { 'authors.full_name': { query: `"${author}"`, operator: 'and' } } });
     });
     if (filters?.startYear && filters?.endYear) {
-      query.query.bool.must.push({ range: { year: { gte: filters.startYear, lte: filters.endYear } } });
+      query.query.bool.filter.push({ range: { year: { gte: filters.startYear, lte: filters.endYear } } });
     } else if (filters?.startYear) {
-      query.query.bool.must.push({ range: { year: { gte: filters.startYear } } });
+      query.query.bool.filter.push({ range: { year: { gte: filters.startYear } } });
     } else if (filters?.endYear) {
-      query.query.bool.must.push({ range: { year: { lte: filters.endYear } } });
+      query.query.bool.filter.push({ range: { year: { lte: filters.endYear } } });
     }
     if (filters.affiliationsToExclude.length > 0 || filters.authorsToExclude.length > 0) {
       query.query.bool.must_not = [];
