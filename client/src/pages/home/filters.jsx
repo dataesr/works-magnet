172bceb4008d1c532dab7a05cc50e3ac807ad44b
Anne L'Hôte <anne.lhote@gmail.com>
2023-07-12 16:38:54 +0200

    feat(queries): Enable multiple datasources

--- client/src/pages/home/filters.jsx
+++ client/src/pages/home/filters.jsx
@@ -147,6 +147,7 @@ export default function Filters({
               authorsToExclude,
               startYear,
               endYear,
+              datasources,
             })}
             size="sm"
           >
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -2,43 +2,38 @@ const {
   VITE_ES_SIZE,
 } = import.meta.env;
 
-const getQuery = ({ datasource, filters }) => {
+const getBsoQuery = ({ filters }) => {
   const query = { size: VITE_ES_SIZE, query: { bool: {} } };
-  switch (datasource) {
-  case 'bso':
-    if (filters.affiliations.length > 0 || filters.authors.length > 0) {
-      query.query.bool.should = [];
-    }
-    filters.affiliations.forEach((affiliation) => {
-      query.query.bool.should.push({ match: { 'affiliations.name': { query: `"${affiliation}"`, operator: 'and' } } });
-    });
-    filters.authors.forEach((author) => {
-      query.query.bool.should.push({ match: { 'authors.full_name': { query: `"${author}"`, operator: 'and' } } });
-    });
-    if (filters.affiliationsToExclude.length > 0 || filters.authorsToExclude.length > 0) {
-      query.query.bool.must_not = [];
-    }
-    filters.affiliationsToExclude.forEach((affiliationToExclude) => {
-      query.query.bool.must_not.push({ match: { 'affiliations.name': { query: affiliationToExclude, operator: 'and' } } });
-    });
-    filters.authorsToExclude.forEach((authorToExclude) => {
-      query.query.bool.must_not.push({ match: { 'authors.full_name': { query: authorToExclude, operator: 'and' } } });
-    });
-    if (filters?.startYear || filters?.endYear) {
-      query.query.bool.filter = [];
-    }
-    if (filters?.startYear && filters?.endYear) {
-      query.query.bool.filter.push({ range: { year: { gte: filters.startYear, lte: filters.endYear } } });
-    } else if (filters?.startYear) {
-      query.query.bool.filter.push({ range: { year: { gte: filters.startYear } } });
-    } else if (filters?.endYear) {
-      query.query.bool.filter.push({ range: { year: { lte: filters.endYear } } });
-    }
-    query.highlight = { fields: { 'affiliations.name': {}, 'authors.full_name': {} } };
-    break;
-  default:
+  if (filters.affiliations.length > 0 || filters.authors.length > 0) {
+    query.query.bool.should = [];
   }
+  filters.affiliations.forEach((affiliation) => {
+    query.query.bool.should.push({ match: { 'affiliations.name': { query: `"${affiliation}"`, operator: 'and' } } });
+  });
+  filters.authors.forEach((author) => {
+    query.query.bool.should.push({ match: { 'authors.full_name': { query: `"${author}"`, operator: 'and' } } });
+  });
+  if (filters.affiliationsToExclude.length > 0 || filters.authorsToExclude.length > 0) {
+    query.query.bool.must_not = [];
+  }
+  filters.affiliationsToExclude.forEach((affiliationToExclude) => {
+    query.query.bool.must_not.push({ match: { 'affiliations.name': { query: affiliationToExclude, operator: 'and' } } });
+  });
+  filters.authorsToExclude.forEach((authorToExclude) => {
+    query.query.bool.must_not.push({ match: { 'authors.full_name': { query: authorToExclude, operator: 'and' } } });
+  });
+  if (filters?.startYear || filters?.endYear) {
+    query.query.bool.filter = [];
+  }
+  if (filters?.startYear && filters?.endYear) {
+    query.query.bool.filter.push({ range: { year: { gte: filters.startYear, lte: filters.endYear } } });
+  } else if (filters?.startYear) {
+    query.query.bool.filter.push({ range: { year: { gte: filters.startYear } } });
+  } else if (filters?.endYear) {
+    query.query.bool.filter.push({ range: { year: { lte: filters.endYear } } });
+  }
+  query.highlight = { fields: { 'affiliations.name': {}, 'authors.full_name': {} } };
   return query;
 };
 
-export default getQuery;
+export default getBsoQuery;
