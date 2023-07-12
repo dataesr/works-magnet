60c348867355cb2939fee15b0f0e4915659c8fe4
jerem <jeremy.peglion@gmail.com>
2023-07-12 11:54:46 +0200

    react-query

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,19 +1,36 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import {
-  Container, Row, Col,
+  Container,
   Table,
 } from '@dataesr/react-dsfr';
+import { useQuery } from '@tanstack/react-query';
 import Filters from './filters';
 import getQuery from '../../utils/queries';
+// import { PageSpinner } from '../../components/spinner';
 
 const {
   VITE_API_URL,
   VITE_API_AUTH,
 } = import.meta.env;
 
+async function getData(options) {
+  const params = {
+    method: 'POST',
+    body: JSON.stringify(getQuery(options)),
+    headers: {
+      'content-type': 'application/json',
+      Authorization: VITE_API_AUTH,
+    },
+  };
+  return fetch(VITE_API_URL, params).then((response) => {
+    if (response.ok) return response.json();
+    return 'Oops... API request did not work';
+  });
+}
+
 export default function Home() {
   const [options, setOptions] = useState({});
-  const [data, setData] = useState([]);
+  const [actions, setActions] = useState([{ doi: '10.1007/s13595-016-0554-5', action: 'keep' }]); // keep || reject
   const columns = [
     { label: 'action', name: 'action' },
     { label: 'doi', name: 'doi' },
@@ -24,47 +41,28 @@ export default function Home() {
     { label: 'affiliations', name: 'affiliations' },
   ];
   const getAffiliationsField = (affiliations) => {
-    const nbAffiliations = affiliations.length;
+    const nbAffiliations = affiliations?.length || 0;
     if (nbAffiliations === 0) return '';
     if (nbAffiliations === 1) return affiliations[0].name;
     return `${affiliations[0].name} et al. (${nbAffiliations - 1})`;
   };
   const getAuthorsField = (authors) => {
-    const nbAuthors = authors.length;
+    const nbAuthors = authors?.length || 0;
     if (nbAuthors === 0) return '';
     if (nbAuthors === 1) return authors[0].full_name;
     return `${authors[0].full_name} et al. (${nbAuthors - 1})`;
   };
 
-  useEffect(() => {
-    const getData = async () => {
-      const params = {
-        method: 'POST',
-        query: JSON.stringify(getQuery(options)),
-        headers: {
-          'content-type': 'application/json',
-          Authorization: VITE_API_AUTH,
-        },
-      };
-      const response = await fetch(VITE_API_URL, params);
-      const jsonResponse = await response.json();
-      const dataTable = jsonResponse.hits.hits.map((item, index) => ({
-        affiliations: getAffiliationsField(item._source.affiliations),
-        authors: getAuthorsField(item._source.authors),
-        doi: item._source.doi,
-        id: index,
-        title: item._source.title,
-        url: item._source.url,
-        year: item._source.year,
-        action: actions.find((action) => action.doi === item._source.doi)?.action,
-      }));
-      setData(dataTable);
-    };
-    if (options.filters) getData();
-  }, [options]);
+  const { data, isFetching, refetch } = useQuery({
+    queryKey: ['data'],
+    queryFn: () => getData(options),
+    enabled: false,
+    staleTime: Infinity,
+    cacheTime: Infinity,
+  });
 
-  const sendQuery = (filters) => {
-    setOptions({
+  const sendQuery = async (filters) => {
+    await setOptions({
       datasource: 'bso',
       filters: {
         affiliations: filters.affiliations,
@@ -75,23 +73,35 @@ export default function Home() {
         endYear: filters.endYear,
       },
     });
+    refetch();
   };
 
-  // keep || reject
-  const actions = [
-    { doi: '10.1007/s13595-016-0554-5', action: 'keep' },
-  ];
+  let dataTable = [];
+  if (data?.hits?.hits) {
+    dataTable = data.hits.hits.map((item, index) => ({
+      affiliations: getAffiliationsField(item._source.affiliations),
+      authors: getAuthorsField(item._source.authors),
+      doi: item._source.doi,
+      id: index,
+      title: item._source.title,
+      url: item._source.url,
+      year: item._source.year,
+      action: actions.find((action) => action.doi === item._source.doi)?.action,
+    }));
+  }
 
   return (
     <Container className="fr-my-5w" as="section">
       <Filters
         sendQuery={sendQuery}
       />
+      {isFetching && (<Container>...</Container>)}
+
       {
-        data && (
+        dataTable && (
           <Table
             columns={columns}
-            data={data}
+            data={dataTable}
             rowKey="undefined"
           />
         )
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,34 +1,34 @@
 const getQuery = ({ datasource, filters }) => {
-  const query = { size: 10000, query: { bool: {} } };
+  const query = { size: 100, query: { bool: {} } };
   switch (datasource) {
-  case 'bso':
-    if (filters.affiliations.length > 0 || filters.authors.length > 0 || filters?.startYear || filters?.endYear) {
-      query.query.bool.must = [];
-    }
-    filters.affiliations.forEach((affiliation) => {
-      query.query.bool.must.push({ match: { 'affiliations.name': { query: affiliation, operator: 'and' } } });
-    });
-    filters.authors.forEach((author) => {
-      query.query.bool.must.push({ match: { 'authors.full_name': { query: author, operator: 'and' } } });
-    });
-    if (filters?.startYear && filters?.endYear) {
-      query.query.bool.must.push({ range: { year: { gte: filters.startYear, lte: filters.endYear } } });
-    } else if (filters?.startYear) {
-      query.query.bool.must.push({ range: { year: { gte: filters.startYear } } });
-    } else if (filters?.endYear) {
-      query.query.bool.must.push({ range: { year: { lte: filters.endYear } } });
-    }
-    if (filters.affiliationsToExclude.length > 0 || filters.authorsToExclude.length > 0) {
-      query.query.bool.must_not = [];
-    }
-    filters.affiliationsToExclude.forEach((affiliationToExclude) => {
-      query.query.bool.must_not.push({ match: { 'affiliations.name': { query: affiliationToExclude, operator: 'and' } } });
-    });
-    filters.authorsToExclude.forEach((authorToExclude) => {
-      query.query.bool.must_not.push({ match: { 'authors.full_name': { query: authorToExclude, operator: 'and' } } });
-    });
-    break;
-  default:
+    case 'bso':
+      if (filters.affiliations.length > 0 || filters.authors.length > 0 || filters?.startYear || filters?.endYear) {
+        query.query.bool.must = [];
+      }
+      filters.affiliations.forEach((affiliation) => {
+        query.query.bool.must.push({ match: { 'affiliations.name': { query: affiliation, operator: 'and' } } });
+      });
+      filters.authors.forEach((author) => {
+        query.query.bool.must.push({ match: { 'authors.full_name': { query: author, operator: 'and' } } });
+      });
+      if (filters?.startYear && filters?.endYear) {
+        query.query.bool.must.push({ range: { year: { gte: filters.startYear, lte: filters.endYear } } });
+      } else if (filters?.startYear) {
+        query.query.bool.must.push({ range: { year: { gte: filters.startYear } } });
+      } else if (filters?.endYear) {
+        query.query.bool.must.push({ range: { year: { lte: filters.endYear } } });
+      }
+      if (filters.affiliationsToExclude.length > 0 || filters.authorsToExclude.length > 0) {
+        query.query.bool.must_not = [];
+      }
+      filters.affiliationsToExclude.forEach((affiliationToExclude) => {
+        query.query.bool.must_not.push({ match: { 'affiliations.name': { query: affiliationToExclude, operator: 'and' } } });
+      });
+      filters.authorsToExclude.forEach((authorToExclude) => {
+        query.query.bool.must_not.push({ match: { 'authors.full_name': { query: authorToExclude, operator: 'and' } } });
+      });
+      break;
+    default:
   }
   return query;
 };
