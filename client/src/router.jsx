98e76ab90827e97dd5ab4e0e5f0783122ef38119
Anne L'Hôte <anne.lhote@gmail.com>
2023-07-14 11:04:09 +0200

    feat(ci): Add Github Action to publish static website to Github Pages

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -2,14 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 
 import Layout from './layout';
 import Home from './pages/home';
-import Swagger from './pages/swagger';
+// import Swagger from './pages/swagger';
 
 export default function Router() {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/swagger" element={<Swagger />} />
+        {/* <Route path="/swagger" element={<Swagger />} /> */}
       </Route>
     </Routes>
   );
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/publications-finder/',
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:3000',
+  //       changeOrigin: true,
+  //       secure: false,
+  //       ws: true,
+  //     },
+  //   },
+  // },
 });
