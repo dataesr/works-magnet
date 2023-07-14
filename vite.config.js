d429f5fd6e25b161d51ecf76caeded80abe518b6
Anne L'Hôte <anne.lhote@gmail.com>
2023-07-14 12:30:23 +0200

    fix(ci): Hopeless commit

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/publications-finder/',
-  // base: './',
+  // base: '/publications-finder/',
+  base: './',
   build: {
     emptyOutDir: true,
     outDir: '../dist'
