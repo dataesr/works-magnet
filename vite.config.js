a8583b69a16f84c31e889c1cc71d047e9508d5c0
Anne L'Hôte <anne.lhote@gmail.com>
2023-08-07 15:48:05 +0200

    fix(ui): Fix base url to download assets, #6

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/works-finder/',
   build: {
     emptyOutDir: true,
     outDir: '../dist'
