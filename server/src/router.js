21b0e9733be8c88a67d0c68ff0aa39803628f250
Anne L'Hôte <anne.lhote@gmail.com>
2023-11-17 09:14:32 +0100

    refactor(back): Clean code

--- server/src/router.js
+++ server/src/router.js
@@ -1,13 +1,9 @@
 import express from 'express';
 
-import bsoRouter from './routes/bso.routes';
-import openalexRouter from './routes/openalex.routes';
 import worksRouter from './routes/works.routes';
 
 const router = new express.Router();
 
-router.use(bsoRouter);
-router.use(openalexRouter);
 router.use(worksRouter);
 
 export default router;
