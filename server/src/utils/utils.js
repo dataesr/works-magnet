2de356b22c9f67324aea3332d15ba8083c07787b
Anne L'Hôte <anne.lhote@gmail.com>
2023-12-06 18:26:08 +0100

    fix(api): Improve the affiliations names normalization

--- server/src/utils/utils.js
+++ server/src/utils/utils.js
@@ -122,7 +122,8 @@ for (let i = 0; i < defaultDiacriticsRemovalMap.length; i += 1) {
 
 // "what?" version ... http://jsperf.com/diacritics/12
 // eslint-disable-next-line no-control-regex
-const removeDiacritics = (str) => str.replace(/[^\u0000-\u007E]/g, (a) => diacriticsMap[a] || a).toLowerCase();
+const removeDiacritics = (str) => str.replace(/[^\u0000-\u007E]/g, (a) => diacriticsMap[a] || a).replace(/[,%().*:;]/g, '').replace(/-/g, ' ').toLowerCase()
+  .trim();
 
 export {
   cleanId,
