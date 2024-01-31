cb702b9625274f43d023b8cc75a3de189e80fe65
Anne L'Hôte <anne.lhote@gmail.com>
2024-01-31 18:08:41 +0100

    feta(api): Add new endpoint to download file

--- server/src/router.js
+++ server/src/router.js
@@ -1,9 +1,11 @@
 import express from 'express';
 
+import filesRouter from './routes/files.routes';
 import worksRouter from './routes/works.routes';
 
 const router = new express.Router();
 
+router.use(filesRouter);
 router.use(worksRouter);
 
 export default router;
---
+++ server/src/routes/files.routes.js
@@ -0,0 +1,24 @@
+import { execSync } from 'child_process';
+import express from 'express';
+
+const router = new express.Router();
+
+const key = process.env.OS_PASSWORD;
+const projectName = process.env.OS_PROJECT_NAME;
+const projectId = process.env.OS_TENANT_ID;
+const tenantName = process.env.OS_TENANT_NAME;
+const username = process.env.OS_USERNAME;
+const user = `${tenantName}:${username}`;
+
+router.route('/download')
+  .get(async (req, res) => {
+    const container = 'works-finder';
+    // eslint-disable-next-line max-len
+    const initCmd = `swift --os-auth-url https://auth.cloud.ovh.net/v3 --auth-version 3 --key ${key} --user ${user} --os-project-domain-name Default --os-project-id ${projectId} --os-project-name ${projectName} --os-region-name GRA`;
+    const cmd = `${initCmd} list ${container} > list_files_${container}`;
+    execSync(cmd);
+    // list_files = [k.strip() for k in open(f'list_files_{container}_{prefix}', 'r').readlines()]
+    res.status(200).send('DONE');
+  });
+
+export default router;
