008586bab0898b704b497c868cbd22e7ce2bcff4
Anne L'Hôte <anne.lhote@gmail.com>
2024-04-16 15:33:45 +0200

    Revert "fix(dsfr-plus): Add DSFR Config"
    This reverts commit ac4bd364c8e2df6930c154d9578d44c6a908c735.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,9 @@
-import { DSFRConfig } from '@dataesr/dsfr-plus';
+import React, { useEffect } from 'react';
+import { BrowserRouter, useLocation } from 'react-router-dom';
 import { createInstance, MatomoProvider, useMatomo } from '@m4tt72/matomo-tracker-react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, RouterLink, useLocation } from 'react-router-dom';
 
 import Router from './router';
 import { ToastContextProvider } from './hooks/useToast';
@@ -41,12 +40,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <BrowserRouter>
         <PageTracker />
         <ToastContextProvider>
-          <DSFRConfig routerComponent={RouterLink}>
-            <QueryClientProvider client={queryClient}>
-              <ReactQueryDevtools />
-              <Router />
-            </QueryClientProvider>
-          </DSFRConfig>
+          <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools />
+            <Router />
+          </QueryClientProvider>
         </ToastContextProvider>
       </BrowserRouter>
     </MatomoProvider>
