6434819fd12ad4509ed298d4417a0421c045d201
Anne L'Hôte <anne.lhote@gmail.com>
2024-04-19 17:05:09 +0200

    fix(openalex): Replace "author.raw_affiliation_string" by "author.raw_affiliation_strings" as this field is deprecated

--- server/src/utils/works.js
+++ server/src/utils/works.js
@@ -301,7 +301,7 @@ const getTypeFromOpenAlex = (type) => {
 
 const getOpenAlexAffiliation = (author) => {
   const source = 'OpenAlex';
-  const rawAffiliation = author.raw_affiliation_string;
+  const rawAffiliation = author.raw_affiliation_strings.concat('; ');
   let key = removeDiacritics(rawAffiliation).concat(' [ source: ').concat(source).concat(' ]');
   const label = removeDiacritics(rawAffiliation).concat(' [ source: ').concat(source).concat(' ]');
   const rors = [];
