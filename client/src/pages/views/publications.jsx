b8374b727bcc61985030b18e3071298e29805d06
Anne L'Hôte <anne.lhote@gmail.com>
2024-05-16 15:29:53 +0200

    fix(publications): Restore button to export publications

--- client/src/pages/views/publications.jsx
+++ client/src/pages/views/publications.jsx
@@ -1,13 +1,16 @@
 /* eslint-disable max-len */
 import {
-  Row, Col,
-  SegmentedControl, SegmentedElement,
+  Row,
+  Col,
+  SegmentedControl,
+  SegmentedElement,
   Title,
 } from '@dataesr/dsfr-plus';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 
 import ActionsAffiliations from '../actions/actionsAffiliations';
+import ActionsPublications from '../actions/actionsPublications';
 import AffiliationsTab from '../affiliationsTab';
 import PublicationsTab from '../publicationsTab';
 
@@ -36,7 +39,8 @@ export default function Publications({
             <Col xs="12">
               <div className="fr-callout fr-mb-16w">
                 <p className="fr-callout__text fr-text--sm">
-                  To search for publications, please disable the "Search for datasets only" option
+                  To search for publications, please disable the "Search for
+                  datasets only" option
                 </p>
               </div>
             </Col>
@@ -69,28 +73,36 @@ export default function Publications({
               </SegmentedControl>
             </Col>
           </Row>
-          {
-            (Tab === 'selectAffiliations') && (
-              <Row>
-                <Col xs="12">
-                  <div className="fr-callout fr-callout--pink-tuile">
-                    <Title as="h3" look="h6">
-                      Select the raw affiliations corresponding to your institution
-                    </Title>
-                    <p className="fr-callout__text fr-text--sm">
-                      🔎 The array below summarizes the most frequent raw affiliation strings retrieved in the French Open Science Monitor data and in OpenAlex for your query.
-                      <br />
-                      🤔 You can validate ✅ or exclude ❌ each of them, whether it actually corresponds to your institution or not. If an affiliation is validated, it will also validate all the works with that affiliation string.
-                      <br />
-                      🤖 The second column indicates the RoR automatically computed by OpenAlex. Sometimes, they can be inaccurate or missing. If any errors, please use the first tab to send feedback.
-                      <br />
-                      💾 You can save (export to a file) those decisions, and restore them later on.
-                    </p>
-                  </div>
-                </Col>
-              </Row>
-            )
-          }
+          {Tab === 'selectAffiliations' && (
+            <Row>
+              <Col xs="12">
+                <div className="fr-callout fr-callout--pink-tuile">
+                  <Title as="h3" look="h6">
+                    Select the raw affiliations corresponding to your
+                    institution
+                  </Title>
+                  <p className="fr-callout__text fr-text--sm">
+                    🔎 The array below summarizes the most frequent raw
+                    affiliation strings retrieved in the French Open Science
+                    Monitor data and in OpenAlex for your query.
+                    <br />
+                    🤔 You can validate ✅ or exclude ❌ each of them, whether
+                    it actually corresponds to your institution or not. If an
+                    affiliation is validated, it will also validate all the
+                    works with that affiliation string.
+                    <br />
+                    🤖 The second column indicates the RoR automatically
+                    computed by OpenAlex. Sometimes, they can be inaccurate or
+                    missing. If any errors, please use the first tab to send
+                    feedback.
+                    <br />
+                    💾 You can save (export to a file) those decisions, and
+                    restore them later on.
+                  </p>
+                </div>
+              </Col>
+            </Row>
+          )}
           <Row>
             <Col>
               <ActionsAffiliations
@@ -101,18 +113,17 @@ export default function Publications({
           </Row>
           <Row>
             <Col xs="12">
-              {
-                (Tab === 'selectAffiliations') && (
-                  <AffiliationsTab
-                    affiliations={allAffiliations}
-                    selectedAffiliations={selectedAffiliations}
-                    setSelectedAffiliations={setSelectedAffiliations}
-                    tagAffiliations={tagAffiliations}
-                  />
-                )
-              }
-              {
-                (Tab === 'listOfPublications') && (
+              {Tab === 'selectAffiliations' && (
+                <AffiliationsTab
+                  affiliations={allAffiliations}
+                  selectedAffiliations={selectedAffiliations}
+                  setSelectedAffiliations={setSelectedAffiliations}
+                  tagAffiliations={tagAffiliations}
+                />
+              )}
+              {Tab === 'listOfPublications' && (
+                <>
+                  <ActionsPublications allPublications={allPublications} />
                   <PublicationsTab
                     publishers={data.publications?.publishers || []}
                     publications={allPublications}
@@ -122,8 +133,8 @@ export default function Publications({
                     types={data.publications?.types || []}
                     years={data.publications?.years || []}
                   />
-                )
-              }
+                </>
+              )}
             </Col>
           </Row>
         </>
@@ -133,13 +144,15 @@ export default function Publications({
 }
 
 Publications.propTypes = {
-  allAffiliations: PropTypes.arrayOf(PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    nameHtml: PropTypes.string.isRequired,
-    status: PropTypes.string.isRequired,
-    works: PropTypes.arrayOf(PropTypes.string).isRequired,
-    worksNumber: PropTypes.number.isRequired,
-  })).isRequired,
+  allAffiliations: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      nameHtml: PropTypes.string.isRequired,
+      status: PropTypes.string.isRequired,
+      works: PropTypes.arrayOf(PropTypes.string).isRequired,
+      worksNumber: PropTypes.number.isRequired,
+    }),
+  ).isRequired,
   setSelectedAffiliations: PropTypes.func.isRequired,
   selectedAffiliations: PropTypes.arrayOf(PropTypes.object).isRequired,
   tagAffiliations: PropTypes.func.isRequired,
