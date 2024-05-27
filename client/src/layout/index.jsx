a5d8349c0189c04fea498ab5b0aa94b306907142
Anne L'Hôte <anne.lhote@gmail.com>
2024-05-27 11:56:32 +0200

    chore(header): Remove SwitchTheme

--- client/src/layout/index.jsx
+++ client/src/layout/index.jsx
@@ -1,19 +1,13 @@
 import { Container } from '@dataesr/dsfr-plus';
-
-// import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import Footer from './Footer';
 import Header from './Header';
 
 export default function Layout() {
-  // TODO: restore SwitchTheme
-  // const [isSwitchThemeOpen, setIsSwitchThemeOpen] = useState(false);
-
   return (
     <>
       <Header />
-      {/* <SwitchTheme isOpen={isSwitchThemeOpen} setIsOpen={setIsSwitchThemeOpen} /> */}
       <Container fluid as="main" role="main">
         <Outlet />
       </Container>
