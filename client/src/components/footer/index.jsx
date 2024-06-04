81ad5818b47e7d5ed37a7f66c3300ddcb57ddf47
Anne L'Hôte <anne.lhote@gmail.com>
2024-06-04 14:26:49 +0200

    feat(footer): Add contact info

---
+++ client/src/components/footer/index.jsx
@@ -0,0 +1,108 @@
+import { Container, Link, Logo } from '@dataesr/dsfr-plus';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const modules = import.meta.glob('./locales/*.json', {
+  eager: true,
+  import: 'default',
+});
+const messages = Object.keys(modules).reduce((acc, key) => {
+  const locale = key.match(/\.\/locales\/(.+)\.json$/)?.[1];
+  if (locale) {
+    return { ...acc, [locale]: modules[key] };
+  }
+  return acc;
+}, {});
+
+export function FooterTop({ children }) {
+  return <div className="fr-footer__top">{children}</div>;
+}
+FooterTop.propTypes = {
+  children: PropTypes.object.isRequired,
+};
+
+export function Footer({ children, fluid = false }) {
+  return (
+    <footer className="fr-footer fr-mt-3w" role="contentinfo" id="footer">
+      <Container fluid={fluid}>{children}</Container>
+    </footer>
+  );
+}
+Footer.defaultProps = {
+  fluid: false,
+};
+Footer.propTypes = {
+  children: PropTypes.object.isRequired,
+  fluid: PropTypes.bool,
+};
+
+export function FooterBottom({
+  children,
+  copy,
+}) {
+  const childs = React.Children.toArray(children);
+  return (
+    <div className="fr-container fr-footer__bottom">
+      <ul className="fr-footer__bottom-list">
+        {childs.map((child, i) => (
+          <li key={i} className="fr-footer__bottom-item">
+            {child}
+          </li>
+        ))}
+      </ul>
+      {copy ? (
+        <div className="fr-footer__bottom-copy">
+          <p>{copy}</p>
+        </div>
+      ) : null}
+    </div>
+  );
+}
+FooterBottom.defaultProps = {
+  copy: undefined,
+};
+FooterBottom.propTypes = {
+  children: PropTypes.object.isRequired,
+  copy: PropTypes.string,
+};
+
+export function FooterBody({
+  children,
+  description,
+}) {
+  const links = React.Children.toArray(children).filter(
+    (child) => React.isValidElement(child) && child.type === Link,
+  );
+  const logo = React.Children.toArray(children).filter(
+    (child) => React.isValidElement(child) && child.type === Logo,
+  )?.[0];
+
+  return (
+    <div className="fr-container fr-footer__body">
+      {logo ? (
+        <div className="fr-footer__brand fr-enlarge-link">{logo}</div>
+      ) : null}
+      <div className="fr-footer__content">
+        {description ? (
+          <p className="fr-footer__content-desc">{description}</p>
+        ) : null}
+        {links.length ? (
+          <ul className="fr-footer__content-list">
+            {links.map((link, i) => (
+              <li key={i} className="fr-footer__content-item">
+                {link}
+              </li>
+            ))}
+          </ul>
+        ) : null}
+      </div>
+    </div>
+  );
+}
+FooterBody.defaultProps = {
+  description: undefined,
+};
+FooterBody.propTypes = {
+  children: PropTypes.object.isRequired,
+  description: PropTypes.string,
+};
--- client/src/layout/Footer.jsx
+++ client/src/layout/Footer.jsx
@@ -1,121 +1,224 @@
-import { Link, Title } from '@dataesr/dsfr-plus';
+import { Col, Container, Link, Logo, Row, Title } from '@dataesr/dsfr-plus';
+import cn from 'classnames';
 
-export default function Footer() {
+import {
+  Footer,
+  FooterBody,
+  FooterBottom,
+  FooterTop,
+} from '../components/footer/index';
+
+const version = import.meta.env.VITE_VERSION;
+
+export default function MainFooter() {
   return (
-    <footer
-      className="fr-footer fr-mt-md-8w fr-pb-md-2w"
-      role="contentinfo"
-      id="footer"
-    >
-      <div className="fr-container">
-        <div className="fr-footer__body">
-          <div className="fr-footer__brand fr-enlarge-link">
-            <a
-              href="/"
-              title="Retour à l’accueil du site - Nom de l’entité (ministère, secrétariat d‘état, gouvernement)"
-            >
-              <p className="fr-logo">
-                MINISTÈRE
-                <br />
-                DE L'ENSEIGNEMENT
-                <br />
-                SUPÉRIEUR
-                <br />
-                ET DE LA RECHERCHE
-              </p>
-            </a>
-          </div>
-          <div className="fr-grid-row fr-grid-row--center">
-            <div style={{ alignItems: 'center', justifyContent: 'center' }}>
-              <Title as="h3" className="fr-footer__top-cat">
-                Contact
-              </Title>
-              <ul className="fr-footer__top-list">
-                <li>
-                  <Link
-                    className="fr-footer__top-link"
-                    href="https://groupes.renater.fr/sympa/info/bso-etablissements"
-                    target="_blank"
+    <Footer fluid>
+      <FooterTop>
+        <Container>
+          <Row gutters verticalAlign="middle">
+            <Col xs={12} lg={4}>
+              <Row horizontalAlign="left">
+                <div className="text-left">
+                  <Title
+                    as="h3"
+                    className={cn('fr-footer__top-cat', 'text-left')}
                   >
-                    <span
-                      className="fr-icon-group-line fr-icon--sm fr-mr-1w"
-                      aria-hidden="true"
-                    />
-                    Mailing list
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="fr-footer__top-link"
-                    href="mailto:bso@recherche.gouv.fr"
-                    target="_blank"
+                    Contact
+                  </Title>
+                  <ul className="fr-footer__top-list">
+                    <li>
+                      <Link
+                        className="fr-footer__top-link"
+                        href="mailto:bso@recherche.gouv.fr"
+                        icon="mail-fill"
+                        iconPosition="left"
+                      >
+                        Email
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        className="fr-footer__top-link"
+                        href="https://groupes.renater.fr/sympa/info/bso-etablissements"
+                        icon="group-fill"
+                        iconPosition="left"
+                      >
+                        Mailing-list
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </Row>
+            </Col>
+            <Col xs={12} lg={4}>
+              <Row horizontalAlign="center">
+                <div className="text-center">
+                  <Title
+                    as="h3"
+                    className={cn('fr-footer__top-cat', 'text-center')}
                   >
-                    <span
-                      className="fr-icon-mail-line fr-icon--sm fr-mr-1w"
-                      aria-hidden="true"
-                    />
-                    Email
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="fr-footer__content">
-            <p className="fr-footer__content-desc">
-              Retrieve the scholarly works of your institution
-            </p>
-            <p className="fr-footer__content-desc">
-              {`v${import.meta.env.VITE_VERSION}`}
-            </p>
-            <ul className="fr-footer__content-list">
-              <li className="fr-footer__content-item">
-                <a
-                  className="fr-footer__content-link"
-                  target="_blank"
-                  rel="noopener external noreferrer"
-                  title="Légifrance - Le service public de la diffusion du droit - nouvelle fenêtre"
-                  href="https://legifrance.gouv.fr"
-                >
-                  legifrance.gouv.fr
-                </a>
-              </li>
-              <li className="fr-footer__content-item">
-                <a
-                  className="fr-footer__content-link"
-                  target="_blank"
-                  rel="noopener external noreferrer"
-                  title="Gouvernement.fr - Site du gouverment français - nouvelle fenêtre"
-                  href="https://gouvernement.fr"
-                >
-                  gouvernement.fr
-                </a>
-              </li>
-              <li className="fr-footer__content-item">
-                <a
-                  className="fr-footer__content-link"
-                  target="_blank"
-                  rel="noopener external noreferrer"
-                  title="service-public.fr - Le site officiel de l'adiminstration française - nouvelle fenêtre"
-                  href="https://service-public.fr"
-                >
-                  service-public.fr
-                </a>
-              </li>
-              <li className="fr-footer__content-item">
-                <a
-                  className="fr-footer__content-link"
-                  target="_blank"
-                  rel="noopener external noreferrer"
-                  title="Data Gouv - Plateforme ouverte des données publiques françaises - nouvelle fenêtre"
-                  href="https://data.gouv.fr"
-                >
-                  data.gouv.fr
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </footer>
+                    Follow us
+                  </Title>
+                  <ul className="fr-footer__top-list">
+                    <li>
+                      <Link
+                        className="fr-footer__top-link"
+                        href="https://x.com/dataESR"
+                        target="_blank"
+                        icon="twitter-x-fill"
+                        iconPosition="left"
+                      >
+                        X
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        className="fr-footer__top-link"
+                        href="https://www.linkedin.com/company/enseignementsup-recherche/mycompany/"
+                        target="_blank"
+                        icon="linkedin-box-fill"
+                        iconPosition="left"
+                      >
+                        Linkedin
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        className="fr-footer__top-link"
+                        href="https://www.facebook.com/enseignementsup.recherche"
+                        target="_blank"
+                        iconPosition="left"
+                        icon="facebook-circle-fill"
+                      >
+                        Facebook
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </Row>
+            </Col>
+            <Col xs={12} lg={4}>
+              <Row horizontalAlign="right">
+                <div className="text-right">
+                  <Title
+                    as="h3"
+                    className={cn('fr-footer__top-cat', 'text-right')}
+                  >
+                    See also
+                  </Title>
+                  <ul className="fr-footer__top-list">
+                    <li>
+                      <Link
+                        className="fr-footer__top-link"
+                        href="https://github.com/dataesr"
+                        target="_blank"
+                      >
+                        GitHub
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        className="fr-footer__top-link"
+                        href="https://curiexplore.enseignementsup-recherche.gouv.fr/"
+                        target="_blank"
+                      >
+                        CurieXplore
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        className="fr-footer__top-link"
+                        href="https://frenchopensciencemonitor.esr.gouv.fr/"
+                        target="_blank"
+                      >
+                        French Open Science Monitor
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        className="fr-footer__top-link"
+                        href="https://data.esr.gouv.fr/EN/"
+                        target="_blank"
+                      >
+                        #dataESR
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        className="fr-footer__top-link"
+                        href="https://data.enseignementsup-recherche.gouv.fr/pages/home/"
+                        target="_blank"
+                      >
+                        Open Data Platform
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </FooterTop>
+      <FooterBody
+        description="Retrieve the scholarly works of your institution"
+      >
+        <Logo
+          splitCharacter="|"
+          text="Ministère|de l'enseignement|supérieur|et de la recherche"
+        />
+        <Link
+          className="fr-footer__content-link"
+          target="_blank"
+          rel="noreferrer noopener external"
+          title="[À MODIFIER - Intitulé] - nouvelle fenêtre"
+          href="https://legifrance.gouv.fr"
+        >
+          legifrance.gouv.fr
+        </Link>
+        <Link
+          className="fr-footer__content-link"
+          target="_blank"
+          rel="noreferrer noopener external"
+          title="[À MODIFIER - Intitulé] - nouvelle fenêtre"
+          href="https://gouvernement.fr"
+        >
+          gouvernement.fr
+        </Link>
+        <Link
+          className="fr-footer__content-link"
+          target="_blank"
+          rel="noreferrer noopener external"
+          title="[À MODIFIER - Intitulé] - nouvelle fenêtre"
+          href="https://service-public.fr"
+        >
+          service-public.fr
+        </Link>
+        <Link
+          className="fr-footer__content-link"
+          target="_blank"
+          rel="noreferrer noopener external"
+          title="[À MODIFIER - Intitulé] - nouvelle fenêtre"
+          href="https://data.gouv.fr"
+        >
+          data.gouv.fr
+        </Link>
+      </FooterBody>
+      <FooterBottom>
+        {/* <Link className="fr-footer__bottom-link" href="/about/accessibility">
+          Accessibility: currently being optimized
+        </Link>
+        <Link className="fr-footer__bottom-link" href="/about/legal-notices">
+          Legal notices
+        </Link> */}
+        <Link
+          target="_blank"
+          rel="noreferer noopenner"
+          className="fr-footer__bottom-link"
+          href={`https://github.com/dataesr/works-magnet/releases/tag/v${version}`}
+        >
+          {`App version v${version}`}
+        </Link>
+      </FooterBottom>
+    </Footer>
   );
 }
