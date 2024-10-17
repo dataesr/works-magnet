fd09e77aa91947209300df48591a92329a5ae59a
Anne L'Hôte <anne.lhote@gmail.com>
2024-10-17 09:51:59 +0200

    fix(mentions): Config server to let the client handle all unknown routes

--- server/src/app.js
+++ server/src/app.js
@@ -13,7 +13,9 @@ const apiSpec = 'src/openapi/api.yml';
 const apiDocument = YAML.load(apiSpec);
 const app = express();
 
-const expressServer = app.listen(process.env.WS_PORT, () => { console.log(`WebSocket server is running on port ${process.env.WS_PORT}`); });
+const expressServer = app.listen(process.env.WS_PORT, () => {
+  console.log(`WebSocket server is running on port ${process.env.WS_PORT}`);
+});
 expressServer.on('upgrade', (request, socket, head) => {
   webSocketServer.handleUpgrade(request, socket, head, (websocket) => {
     webSocketServer.emit('connection', websocket, request);
@@ -24,22 +26,40 @@ app.use(express.json({ limit: 52428800 }));
 app.use(express.urlencoded({ extended: false }));
 app.disable('x-powered-by');
 if (process.env.NODE_ENV === 'development') {
-  app.use(cors({ origin: '*', methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'] }));
+  app.use(
+    cors({
+      origin: '*',
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    }),
+  );
 } else {
   app.use(express.static(path.join(path.resolve(), 'dist')));
 }
 
-app.get('/api/docs/specs.json', (req, res) => { res.status(200).json(apiDocument); });
+app.get('/api/docs/specs.json', (_, res) => {
+  res.status(200).json(apiDocument);
+});
 
-app.use(OAV.middleware({
-  apiSpec,
-  validateRequests: { removeAdditional: true },
-  validateResponses: true,
-  ignoreUndocumented: true,
-}));
+app.use(
+  OAV.middleware({
+    apiSpec,
+    validateRequests: { removeAdditional: true },
+    validateResponses: true,
+    ignoreUndocumented: true,
+  }),
+);
 
 app.use('/api', router);
 
 app.use(handleErrors);
+
+// https://ui.dev/react-router-cannot-get-url-refresh
+app.get('/*', (_, res) => {
+  res.sendFile(path.join(path.resolve(), 'dist', 'index.html'), (err) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
 
 export default app;
