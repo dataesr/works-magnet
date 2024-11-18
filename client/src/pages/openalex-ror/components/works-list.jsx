70ead63c6baea5dc7e9d9a71a5d51bb400988818
jerem <jeremy.peglion@gmail.com>
2024-11-18 11:18:51 +0100

    style(openalex): style works

--- client/src/pages/openalex-ror/components/works-list.jsx
+++ client/src/pages/openalex-ror/components/works-list.jsx
@@ -6,23 +6,25 @@ export default function WorksList({ works }) {
 
   const _works = showMore ? works : works.slice(0, 5);
   return (
-    <div>
-      <span className="fr-mr-1w">
-        openAlex works:
-      </span>
-      {_works.map((work) => (
-        <Link className="fr-mr-1w" href={`https://openalex.org/${work}`} target="_blank">
-          {work}
-        </Link>
-      ))}
-      {
-        works.length > 5 && (
-          <Button variant="text" onClick={() => setShowMore(!showMore)}>
-            {showMore ? 'show less works' : `show more works (${works.length - 5})`}
-          </Button>
-        )
-      }
-    </div>
+    <Text size="sm" className="fr-mt-1w fr-pl-1w" style={{ borderLeft: '5px solid #aaa' }}>
+      <i>
+        <span className="fr-mr-1w">
+          works:
+        </span>
+        {_works.map((work) => (
+          <Link className="fr-mr-1w" href={`https://openalex.org/${work}`} target="_blank">
+            {work}
+          </Link>
+        ))}
+        {
+          works.length > 5 && (
+            <Button variant="text" onClick={() => setShowMore(!showMore)}>
+              {showMore ? 'show less works' : `show more works (${works.length - 5})`}
+            </Button>
+          )
+        }
+      </i>
+    </Text>
 
   );
 }
