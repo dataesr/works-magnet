16df7d77359a1dfb49d9aaf80a660ef428e8d3ff
Anne L'Hôte <anne.lhote@gmail.com>
2024-12-24 16:00:48 +0100

    feat(mentions): Separate mentions router

--- server/src/router.js
+++ server/src/router.js
@@ -1,11 +1,13 @@
 import express from 'express';
 
 import filesRouter from './routes/files.routes';
+import mentionsRouter from './routes/mentions.routes';
 import worksRouter from './routes/works.routes';
 
 const router = new express.Router();
 
 router.use(filesRouter);
+router.use(mentionsRouter);
 router.use(worksRouter);
 
 export default router;
